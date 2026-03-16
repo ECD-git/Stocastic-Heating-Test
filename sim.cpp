82bf7caeb2099027067f9b284cbd37e39d953bf0
Ethan Duckmanton <ecd@Ethans-MacBook-Air-9.local>
2026-03-16 16:19:22 +0000

    added maxwell-boltzman dist

--- sim.cpp
+++ sim.cpp
@@ -1,3 +1,5 @@
+// No AI used in the making of this im a real one 
+// I get things abhorently wrong the old fasioned way
 #include<iostream>
 #include<cmath>
 #include<vector>
@@ -26,6 +28,7 @@ float RandomFloat(float min, float max)
     // generates a random float between a range min and max
     return min + static_cast <float> (std::rand()) / (static_cast <float> (RAND_MAX/(max-min)));
 }
+
 // Potential Functions
 float PotentialSinSqr(float x, float t, float A, float k, float omega, float phi)
 {
@@ -46,17 +49,38 @@ float ForceSinSqr(float x, float t, float A, float k, float omega, float phi)
    return k*A*std::sin(2*((k*x)+(omega*t)+phi));
 }
 
+// Numerical Methods for getting roots of maxwell-boltzman dist off set by random number
+float MaxBoltCDF(float v, float T)
+{
+    /*
+    Returns the cdf value for the maxwell-boltzman distribution for a temp T and a vel v
+    Assuming a const mass defined at the top of the program
+    */
+    float a = pow((k_B*T)/M,0.5)
+    return std::erf(v/(pow(2,0.5)*a)) - pow(2/M_PI,0.5)*(v/a)*std::exp(-(pow(x,2)/(2*pow(a,2))));
+}
+float MaxBoltPDF(float v, float T)
+{
+    /*
+    returns pdf of maxwell boltz dist -> derivative of cdf (hint hint newton raphson)
+    */
+    float a = pow((k_B*T)/M,0.5)
+    return pow(2/M_PI, 0.5) * (pow(v,2)/pow(a,3)) * std::exp(-(pow(x,2)/(2*pow(a,2))));
+}
+
 // main
 int main()
 {
     std::srand(time(0)); // seed uniform random num generator
     // im seeding with local machine time here, which doesnt give complete randomness but should be sufficient for this
 
-    std::vector<float> test = LinSpace(1,5,5);
-    for(const auto& e : test)
+    std::vector<float> test{{RandomFloat(0,1), RandomFloat(0,1)}};
+    for(const auto& i : test)
     {
-        std::cout<<e<<' ';
+        std::cout<<i<<' ';
     }
     std::cout<<std::endl;
+
+        
 
 }
